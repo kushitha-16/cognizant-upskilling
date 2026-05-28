acd400e8da69c0f73c5ee7bdf57b862de6beaef2
dt23cs123 <meghagowdaa27@gmail.com>
2026-05-28 17:17:56 +0530

    Added first batch of Module 3 Java basics

--- Module_3_Core_Java/01_Basics/_01_HelloWorld.java
+++ Module_3_Core_Java/01_Basics/_01_HelloWorld.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
+public class _01_HelloWorld {
+
     public static void main(String[] args) {
+
         System.out.println("Hello, Cognizant Digital Nurture 5.0");
+
     }
 }
